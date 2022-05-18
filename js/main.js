3c6dc79a85de35e857d932651a9a2fbd08eed3d6
Abdullabek-web-develpe <Abdullabek710@gmail.com>
2022-05-19 01:00:29 +0300

    main folder

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const quoText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn = document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
+
+function randomQuote(){
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  //fetching random quotes/data from the API and parsing it into Javascript object
+  fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+    quoText.innerText = result.content;
+    authorName.innerText = result.author;
+    quoteBtn.innerText = "New Quote";
+    quoteBtn.classList.remove("loading");
+  })
+}
+
+soundBtn.addEventListener("click", () => {
+    let utterance = new SpeechSynthesisUtterance(`${quoText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(quoText.innerText);
+});
+
+twitterBtn.addEventListener("click", () => {
+  let tweetUrl = `https://twitter.com/intent/tweet?url=${quoText.innerText}`;
+  window.open(tweetUrl, "_blank");
+});
+
+
+quoteBtn.addEventListener("click", randomQuote)
